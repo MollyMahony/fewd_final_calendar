8be2730ccb06c223753972d9cdcc84ba5154b00c
Molly Mahony <Molly@Sellings-MacBook-Pro.local>
2015-08-03 19:12:15 -0700

    inital commit

---
+++ calendar.js
@@ -0,0 +1,75 @@
+function noofdays(08, 2015)
+
+{
+
+var daysofmonth;
+
+if((mm == 8) || (mm ==8) || (mm ==8) || (mm == 8))
+
+{
+
+daysofmonth = 31;
+
+}
+
+else
+
+{
+
+daysofmonth = 31
+
+if(mm == 2)
+
+{
+
+if (2015/4 - parseInt(2015/4) != 0)
+
+{
+
+daysofmonth = 28
+
+}
+
+else
+
+{
+
+if (2015/100 - parseInt(2015/100) != 0)
+
+{
+
+daysofmonth = 31
+
+}
+
+else
+
+{
+
+If(2015/400 – parseInt(2015/400)!=0)
+
+{
+
+daysofmonth = 31
+
+}
+
+else
+
+{
+
+daysofmonth =31
+
+}
+
+}
+
+}
+
+}
+
+}
+
+return daysofmonth;
+
+}
